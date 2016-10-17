981ca1f346efe2bd64156a4c2d84af9ace5303aa
Saul <Saul@sauls-macbook-pro.local>
2016-10-16 23:03:26 -0500

    Initial Commit

---
+++ Semana_2Tests/Semana_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Semana_2Tests.swift
+//  Semana_2Tests
+//
+//  Created by Saul on 16/10/16.
+//  Copyright © 2016 S&A. All rights reserved.
+//
+
+import XCTest
+@testable import Semana_2
+
+class Semana_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
